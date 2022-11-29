52b03b140b35547dff9d0cc6941a471add49458f
taisamfo <118145768+taisamfo@users.noreply.github.com>
2022-11-29 19:02:17 -0300

    js

---
+++ script-um.js
@@ -0,0 +1,16 @@
+const mario = document.querySelector('.mario');
+const pipe = document.querySelector('.pipe');
+
+const jump = () =>{
+    mario.classList.add('jump');
+    setTimeout(() => {
+mario.classList.remove('jump');
+    },500);
+}
+     
+ 
+
+
+ 
+
+document.addEventListener('keydown',jump);
